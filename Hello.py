18c91866a1da4b2f1e46f17ea1bad1e91fdc6bbe
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-07-13 20:03:10 +0100

    Initial commit, add function "name_greetings"

--- Hello.py
+++ Hello.py
@@ -1,6 +1,5 @@
-row = input("\nHi! What's your name?\n").capitalize()
-
 def name_greetings():
+    row = input("\nHi! What's your name?\n").capitalize()
     print("Hello, {}".format(row))
     return "The end of the questanary"
 
