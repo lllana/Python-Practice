18ff523b0880333d0d821b99eb0edf2c221cfb78
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-07-14 19:06:56 +0100

    Add a room-area calculator

--- Room_Area.py
+++ Room_Area.py
@@ -1,1 +1,7 @@
- 
+greetings = str(input("Hi! Would you like to calculate you room area? type YES or NO .... ")).lower
+
+weight = float(input("Type the weight of your room (please, use format: 2.4 (in meteres)...  "))
+length = float(input("Type the length of your room (please, use format: 2.4 (in meteres).... "))
+result = weight*length
+   
+print("Your room area is {} meters".format(result))
