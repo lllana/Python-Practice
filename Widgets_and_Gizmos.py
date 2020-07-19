40ac2aa58a9025408facb170c672d017236b605c
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-07-19 17:45:58 +0100

    add computing the total weight of the order

--- Widgets_and_Gizmos.py
+++ Widgets_and_Gizmos.py
@@ -3,6 +3,14 @@
 # the number of gizmos in an order from the user. Then your program
 # should compute and display the total weight of the order.
 
-widgets =  
+greetings = input("Hi! If you would like to buy widgets and gizmos press ENTER")
 
-gizmos = 
+widgets =  int(input("How many widgets would you like to buy? Please type a whole number(3/5/7 etc.) ... "))
+widgets_weight = widgets * 75
+
+gizmos = int(input("How many gizmos would you like to buy? Please type a whole number(3/5/7 etc.) ... "))
+gizmos_weight = gizmos * 112
+
+total_weight = widgets_weight + gizmos_weight
+
+print('The total weight of your order is {} grams'.format(total_weight))
