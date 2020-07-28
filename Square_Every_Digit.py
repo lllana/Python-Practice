d7a589aa36fef05d3827342868e797e71d86c4b4
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-07-28 22:04:03 +0100

    add 2 more options of the function

--- Square_Every_Digit.py
+++ Square_Every_Digit.py
@@ -1,7 +1,7 @@
 #You are asked to square every digit of a number.
 #Note: The function accepts an integer and returns an integer
 
-def square_digits(num):
+def square_digits1(num):
     digit_list = [int(d) for d in str(num)]
     total_sq_n = 1
     for a in digit_list:
@@ -9,4 +9,29 @@ def square_digits(num):
         total_sq_n *= sq_n
     return total_sq_n    
 
-print(square_digits(9119))
+print(square_digits1(9119))
+
+def square_digits2(num):
+    digit_list = [int(d) for d in str(num)]
+    total_sq_n = []
+    for a in digit_list:
+        sq_n = a ** 2
+        total_sq_n.append(sq_n)
+    
+    for b in total_sq_n:
+        c = [str(b) for b in total_sq_n] 
+        res = int("".join(c))
+    return res    
+
+print(square_digits2(9191))
+
+def square_digits3(num):
+    digit_list = [int(d) for d in str(num)]
+    total_sq_n = []
+    for a in digit_list:
+        sq_n = a ** 2
+        total_sq_n.append(str(sq_n))
+    
+    return int("".join(total_sq_n))   
+
+print(square_digits3(9191))
