80a6591db380d844cd7a50be351763bf949e6927
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-09-27 01:34:15 +0100

    add calling element from nested list

---
+++ Yandex_Practicum/call_element_from_nested_list.py
@@ -0,0 +1,13 @@
+#Get the quantity of the “Tears of Joy” emoji on Instagram from the table and print the result.
+#['Tears of Joy'] [7.31]
+
+data = [
+    ['Grinning', 2.26, 1.02, 87.3], 
+    ['Beaming', 19.1, 1.69, 150.0], 
+    ['ROFL', 25.6, 0.774, 0.0], 
+    ['Tears of Joy', 233.0, 7.31, 2270.0], 
+    ['Winking', 15.2, 2.36, 264.0]
+]
+
+# < write code here >
+print(data[3][2])
