8ee9165a908f338d8332fb9163c4f3b37b73bd66
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-09-30 17:22:16 +0100

    add iteration through list to build a dictionary

--- analytics_data_scv_read.py
+++ analytics_data_scv_read.py
@@ -1,6 +1,26 @@
 import pandas as pd 
 
-col_list = ['Campaign', 'Event Action', 'New Users']
-df = pd.read_csv('campaign_events.csv', skiprows = 6, usecols = col_list)
-print(df.head(24))
+#Read csv files and print needed colomns
+
+csv_file = 'campaign_events.csv'
+row_number = 38
+col_list = ['Campaign', 'Event Action', 'New Users', 'Avg. Session Duration']
+
+def read_file(csv_file):
+    df = pd.read_csv(csv_file, skiprows = 6, usecols = col_list)
+    result = df.head(row_number)
+    
+    
+#campaign dictionary form
+    campaign_dic = {}
+    for i in result['Campaign']:
+        if i in campaign_dic.keys() == True:
+            pass
+        else:
+            campaign_dic[i] = 0
+
+
+    return campaign_dic
+
+print(read_file(csv_file))
 
