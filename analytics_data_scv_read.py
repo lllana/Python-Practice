2599dae63ffe243bf645bb0fb8e0ead01e49ff75
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-01 08:58:53 +0100

    Change name download btn in list

--- analytics_data_scv_read.py
+++ analytics_data_scv_read.py
@@ -5,23 +5,24 @@ import pandas as pd
 csv_file = 'campaign_events.csv'
 row_number = 38
 col_list = ['Campaign', 'Event Action', 'New Users', 'Avg. Session Duration']
-dwnd_btn = ['Free start click', 'Main \'Free Start\' Click', '\'Add Player\' click', 'Upper Download click', 'Lower Download click']
+dwnd_btn = ['Free start click', 'Main \'Free Start\' Click', 'Add Player\' click', 'Upper Download click', 'Lower Download click']
 
 def read_file(csv_file):
     df = pd.read_csv(csv_file, skiprows = 6, usecols = col_list)
     result = df.head(row_number)
     
-    
-#campaign dictionary form
+    #campaign dictionary form
     campaign_dic = {}
-    for i in result['Campaign']:
-        if i in campaign_dic.keys() == True:
-            pass
-        else:
-            campaign_dic[i] = 0
-    
-    #for i in result:
-        #if i in campaign_dic and result['Event Action'] = 
+    for _, row in result.iterrows():
+        campaign_name = row['Campaign']
+        action_name = row['Event Action']
+        new_users = row['New Users']    
+
+        if action_name in dwnd_btn:
+            if campaign_name in campaign_dic:
+                campaign_dic[campaign_name] += new_users 
+            else:
+                campaign_dic[campaign_name] = new_users
 
     return campaign_dic
 
