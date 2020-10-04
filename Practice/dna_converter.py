fd701f7dc55585448ed88e6215ad20cf3002be14
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-04 20:19:17 +0100

    Add 2 version of dna converter function

--- Practice/dna_converter.py
+++ Practice/dna_converter.py
@@ -1,5 +1,6 @@
+#First attempt 
 def DNA_strand(dna):
-    
+
     result = ''
     for i in dna:
         if i == 'A':
@@ -10,7 +11,29 @@ def DNA_strand(dna):
             result += 'G'
         elif i == 'G':
             result += 'C'
-            
+     
     return result
 
 print(DNA_strand('GTAG'))
+
+
+def DNA_strand(dna):
+
+    dict = {'A': 'T', 'T':'A', 'G':'C', 'C':'G'}    
+    result = '' 
+    
+    for i in dna:
+        result += dict[i]
+        
+    return result
+
+print(DNA_strand('GTAG'))
+
+
+def DNA_strand(dna):
+
+    dict = {'A': 'T', 'T':'A', 'G':'C', 'C':'G'}    
+        
+    return ''.join([dict[i] for i in dna])
+
+print(DNA_strand('GTAG'))    
