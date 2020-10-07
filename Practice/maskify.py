99d90b0aa449ff6c47cd747f9293c596325981bb
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-07 23:13:16 +0100

    Add two more solutions

--- Practice/maskify.py
+++ Practice/maskify.py
@@ -1,5 +1,6 @@
 # Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
+#1option
 def maskify(cc):
 
     maskify = ''
@@ -8,5 +9,19 @@ def maskify(cc):
         maskify += i
         
     return maskify + cc[-4:]
+
+print(maskify('636363636636636666'))
+
+
+#2option
+def maskify(cc):
+    return ''.join([i.replace(i,'#') for i in cc[:-4]]) + cc[-4:]
+
+print(maskify('636363636636636666'))
+
+
+#3option
+def maskify(cc):
+    return "#"*(len(cc)-4) + cc[-4:]
 
 print(maskify('636363636636636666'))
