d5f36d089d075cf4b9c1ca5b584eb59964a76c2c
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-07 23:15:07 +0100

    check alignment

--- Practice/maskify.py
+++ Practice/maskify.py
@@ -1,6 +1,6 @@
 # Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
-#1option
+# 1 option
 def maskify(cc):
 
     maskify = ''
@@ -13,14 +13,14 @@ def maskify(cc):
 print(maskify('636363636636636666'))
 
 
-#2option
+# 2 option
 def maskify(cc):
     return ''.join([i.replace(i,'#') for i in cc[:-4]]) + cc[-4:]
 
 print(maskify('636363636636636666'))
 
 
-#3option
+# 3 option
 def maskify(cc):
     return "#"*(len(cc)-4) + cc[-4:]
 
