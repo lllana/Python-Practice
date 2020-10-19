560cb1a429710b3b9c3b179f884687cfac55764d
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-19 23:02:23 +0100

    add second solution with case usage

--- Practice/likes_counter.py
+++ Practice/likes_counter.py
@@ -1,6 +1,6 @@
 #1 option
 
-def likes(names):
+def like_v1(names):
     names_len = len(names)
     
     if names_len == 0:
@@ -16,3 +16,18 @@ def likes(names):
         result ='{}, {} and {} others like this'.format(names[0], names[1], count)
     return result
 
+print(like_v1(['Max', 'John', 'Mark']))
+
+#2 option
+def like_v2(names):
+    names_len = len(names)
+
+    cases = {
+        0: lambda: 'no one likes this',
+        1: lambda: '{} likes this'.format(names[0]),
+        2: lambda: '{} and {} like this'.format(names[0], names[1]),
+        3: lambda: '{}, {} and {} like this'.format(names[0], names[1], names[2]),
+    }
+    return cases.get(names_len, lambda: '{}, {} and {} others like this'.format(names[0], names[1], names_len - 2))()
+    
+print(like_v2(['Max', 'John', 'Mark']))
