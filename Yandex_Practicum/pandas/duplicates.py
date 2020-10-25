46bf9fe71e6f28478cacfc420fe35c9d16538196
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-25 20:56:28 +0000

    add count value in df with condition inside

--- Yandex_Practicum/pandas/duplicates.py
+++ Yandex_Practicum/pandas/duplicates.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 df = pd.read_csv('music_log_upd_nan.csv')
 
+#delete duplicates
 df = df.drop_duplicates().reset_index(drop=True)
 
+#looking for unique names in the df
 print(df['genre_name'].unique())
 
+#replace values in a column
 df['genre_name'] = df['genre_name'].replace('электроника', 'electronic')
+
+#count instances of value in a table with condition inside
+count_variable = df[df['genre_name'] == 'selected_value']['genre_name'].count() 
+print(count_variable)
