07991eae6fb8403ea916c97d8828fa91d0f72996
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-25 20:50:59 +0000

    add replacement of values in a column

--- Yandex_Practicum/pandas/duplicates.py
+++ Yandex_Practicum/pandas/duplicates.py
@@ -5,3 +5,5 @@ df = pd.read_csv('music_log_upd_nan.csv')
 df = df.drop_duplicates().reset_index(drop=True)
 
 print(df['genre_name'].unique())
+
+df['genre_name'] = df['genre_name'].replace('электроника', 'electronic')
