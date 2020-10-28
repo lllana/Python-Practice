5e4cfc5669576e555367d2d254d2919e0c5bd426
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-28 21:22:36 +0000

    add descriptive stat calculations

---
+++ Yandex_Practicum/pandas/descriptive_stat.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df = pd.read_csv('/datasets/music_log_upd_en.csv')
+
+pop_music = df[(df['genre_name'] == 'Pop') & (df['total_play_seconds'] > 0)]
+
+pop_music_max_total_play = pop_music['total_play_seconds'].max()
+#print(pop_music_max_total_play)
+
+pop_music_max_info = pop_music[pop_music['total_play_seconds'] == pop_music_max_total_play]
+print(pop_music_max_info)
