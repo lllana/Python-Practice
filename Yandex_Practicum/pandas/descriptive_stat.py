c55971ef09206bfd57ea476c01920546a4c42c34
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-10-28 21:41:33 +0000

    add median calculation

--- Yandex_Practicum/pandas/descriptive_stat.py
+++ Yandex_Practicum/pandas/descriptive_stat.py
@@ -14,3 +14,6 @@ pop_music_min_total_play = pop_music['total_play_seconds'].min()
 
 pop_music_min_info = pop_music[pop_music['total_play_seconds'] == pop_music_min_total_play]
 #print(pop_music_min_info)
+
+pop_music_median = pop_music['total_play_seconds'].median()
+#print(pop_music_median)
