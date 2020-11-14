d45c95a76b7b0ea11154564d64aec987351c48ba
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-11-14 17:33:28 +0300

    add value_count method

--- Yandex_Practicum/pandas/look_for_duplicates.py
+++ Yandex_Practicum/pandas/look_for_duplicates.py
@@ -2,3 +2,4 @@ import pandas as pd
 stock = pd.read_excel('/datasets/stock_eng.xlsx', sheet_name='storehouse')
 print ('Duplicate entries in the table:', stock['item'].duplicated().sum())
 
+print(stock['item'].value_counts())
