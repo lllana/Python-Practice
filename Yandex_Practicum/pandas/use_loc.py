0465f058a1ce5c73e7cdde36c15d03c83af016c1
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-11-14 17:46:17 +0300

    add loc function - replace with new value

---
+++ Yandex_Practicum/pandas/use_loc.py
@@ -0,0 +1,10 @@
+import pandas as pd
+stock = pd.read_excel('/datasets/stock_eng.xlsx', sheet_name='storehouse')
+xiaomi = stock[stock['item'] == 'Xiaomi Redmi 6A 16GB']['count'].sum()
+huawei = stock[stock['item'] == 'HUAWEI P30 lite']['count'].sum()
+stock['item'] = stock['item'].drop_duplicates()
+stock = stock.dropna().reset_index(drop=True)
+
+stock.loc[stock['item'] == 'Xiaomi Redmi 6A 16GB', 'count'] = xiaomi
+
+print(stock)
