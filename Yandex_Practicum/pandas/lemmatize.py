61a6c9bf0dc7d38c91647763ef3d91633ed5518c
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-11-14 21:33:50 +0300

    add lematize

---
+++ Yandex_Practicum/pandas/lemmatize.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+feedback=pd.read_csv('/datasets/feedback_eng.csv')
+wordnet_lemma = WordNetLemmatizer()
+
+words = nltk.word_tokenize(feedback['text'][1])
+
+lemmas = [wordnet_lemma.lemmatize(w, pos = 'n') for w in words]
+
+print(lemmas)
