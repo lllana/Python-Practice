4db6aafda387305d7e5aba3e4c8b92e375c67bba
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-11-14 21:39:36 +0300

    add counter

--- Yandex_Practicum/pandas/lemmatize.py
+++ Yandex_Practicum/pandas/lemmatize.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
 import pandas as pd
+from collections import Counter
+
 feedback=pd.read_csv('/datasets/feedback_eng.csv')
 wordnet_lemma = WordNetLemmatizer()
 
@@ -9,3 +11,4 @@ words = nltk.word_tokenize(feedback['text'][1])
 lemmas = [wordnet_lemma.lemmatize(w, pos = 'n') for w in words]
 
 print(lemmas)
+print(Counter(lemmas))
