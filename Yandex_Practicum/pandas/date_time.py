0d7b797293f7be85541b0965f5b9e6993ca92bbb
Lana_Rubchenia <svetarubchenya@gmail.com>
2020-12-06 20:41:38 +0000

    add time zone change

--- Yandex_Practicum/pandas/date_time.py
+++ Yandex_Practicum/pandas/date_time.py
@@ -5,3 +5,9 @@ data = pd.read_csv('/datasets/visits_eng.csv', sep='\t')
 data['date_time'] = pd.to_datetime(data['date_time'], format = '%Y-%m-%dT%H:%M:%S')
 
 print(data.head())
+
+#change of time zone
+
+data['local_time'] = data['date_time'] - pd.Timedelta(hours=7)
+
+print(data.head())
